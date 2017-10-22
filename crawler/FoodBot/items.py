28c1424d0f2a9c9e2ae350c7092e76603c5a1c2f
Amin Zabardast <aminzabardast@gmail.com>
2017-10-23 00:09:23 +0200

    #2. Creating Source1 Recipe Object.

--- crawler/FoodBot/items.py
+++ crawler/FoodBot/items.py
@@ -1,14 +1,46 @@
-# -*- coding: utf-8 -*-
+from FoodBot.settings import SOURCE1
+import json
+from scrapy.selector import Selector
+import re
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+class Source1Recipe():
+    def __init__(self, ID, PageContent):
+        self._page_content = PageContent
+        self.image_address_regex = re.compile(r'(.*)\?', re.IGNORECASE)
+        self.price_regex = re.compile(r"price:\s*<strong>(.*)</strong>", re.IGNORECASE | re.MULTILINE)
 
-import scrapy
+        self._data = {
+            'id':ID,
+            'url': SOURCE1['recipes']+ID,
+            'image': None,
+            'name': None,
+            'time': None,
+            'serves': None,
+            'price': None,
+            'energy': None,
+            'fat': None,
+            'saturated_fat': None,
+            'sugar': None,
+            'salt': None,
+            'ingredients':None
+        }
+        self._process_content()
+
+    def _process_content(self):
+        selector = Selector(text=self._page_content)
+        self._data['image'] = selector.xpath('//div[@class="article--image"]//picture/source/@srcset').re(self.image_address_regex)[0]
+        self._data['name'] = selector.xpath('//h1[@itemprop="name"]/text()').extract()[0]
+        self._data['time'] = selector.xpath('//strong[@itemprop="totalTime"]/text()').extract()[0]
+        self._data['serves'] = selector.xpath('//strong[@itemprop="recipeYield"]/text()').extract()[0]
+        self._data['price'] = selector.xpath('//div[contains(@class,"article__lhs--recipe-info")]').re(self.price_regex)[0]
+        nutrition_ul = selector.xpath('//div[@itemprop="nutrition"]/ul')
+        self._data['energy'] = selector.xpath('//span[@itemprop="calories"]/em/text()').extract()[0]
+        self._data['fat'] = selector.xpath('//meta[@itemprop="fatContent"]/@content').extract()[0]
+        self._data['saturated_fat'] = selector.xpath('//meta[@itemprop="saturatedFatContent"]/@content').extract()[0]
+        self._data['sugar'] = selector.xpath('//meta[@itemprop="sugarContent"]/@content').extract()[0]
+        self._data['salt'] = nutrition_ul.xpath('//li[5]/span/em/text()').extract()[0]
+        self._data['ingredients'] = selector.xpath('//span[@itemprop="ingredients"]/text()').extract()
 
 
-class FoodbotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    def return_json(self):
+        return json.dumps(self._data)
