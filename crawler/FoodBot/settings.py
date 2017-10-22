55f7ea83eaa3b8a0cb1104c76f9c051c4b19f564
Amin Zabardast <aminzabardast@gmail.com>
2017-10-23 00:26:48 +0200

    #2. Minor Changes to match PEP8

--- crawler/FoodBot/settings.py
+++ crawler/FoodBot/settings.py
@@ -8,9 +8,9 @@ USER_AGENT = 'FoodBot'
 ROBOTSTXT_OBEY = True
 
 SOURCE1 = {
-    'url':'https://www.asdagoodliving.co.uk/',
-    'recipes':'https://www.asdagoodliving.co.uk/food/recipes/',
-    'directory':'recipes/source1/'
+    'url': 'https://www.asdagoodliving.co.uk/',
+    'recipes': 'https://www.asdagoodliving.co.uk/food/recipes/',
+    'directory': 'recipes/source1/'
 }
 
 CONCURRENT_REQUESTS = 16
