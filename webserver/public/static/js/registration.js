62505c2df87dc4018aa6c59c0c74339aa7cae3b7
Amin Zabardast <aminzabardast@gmail.com>
2017-12-10 22:21:21 +0100

    #45. Enabling Registration for all users.

---
+++ webserver/public/static/js/registration.js
@@ -0,0 +1,66 @@
+(function($, document, window) {
+
+  var ROOT = 'http://127.0.0.1:5000/';
+
+    $.ajax({
+        url: ROOT+'api/users/',
+        crossDomain: true,
+        error: function(a) {
+            alert("AJAX Error");
+            console.log(a)
+        },
+        dataType: 'json',
+        success: function(data) {
+            if(data._id === undefined){
+                $("body").removeClass("hide-container");
+            }else{
+                window.location.replace("../");
+            }
+        },
+        type: 'GET'
+    });
+
+    $(document).ready(function () {
+        $("#register-form").on('submit',function (e) {
+            e.preventDefault();
+
+            var username = $("form#register-form #username").val().toLowerCase();
+
+            var email = $("form#register-form #email").val().toLowerCase();
+            var reEmail = $("form#register-form #re-email").val().toLowerCase();
+            var emailRegex = /(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/;
+
+            var password = $("form#register-form #password").val();
+            var rePassword = $("form#register-form #re-password").val();
+
+            if(!emailRegex.test(email) || !emailRegex.test(reEmail)){
+                alert("Input email is invalid.");
+            }else if(email != reEmail){
+                alert("Emails do not match.");
+            }else if(password != rePassword){
+                alert("Passwords do not match.");
+            }else{
+                data = {"user_name":username, "email":email, "password":password};
+                $.ajax({
+                    url:ROOT+'api/users/',
+                    crossDomain: true,
+                    error: function (e) {
+                        alert("Ajax Error")
+                    },
+                    data:JSON.stringify(data),
+                    dataType:'json',
+                    success: function (data) {
+                        if(data._id === undefined){
+                            alert(data["error"])
+                        }else{
+                            alert("Successful registration.");
+                            window.location.replace("../login/");
+                        }
+                    },
+                    type: 'POST'
+                });
+            }
+        });
+    });
+
+})(jQuery, document, window);
