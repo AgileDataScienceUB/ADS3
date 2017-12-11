2398262b7e57248f9b23c5ab4f52d49fe1c25e21
Amin Zabardast <aminzabardast@gmail.com>
2017-12-11 23:41:31 +0100

    Changing dummy data structure.

--- webserver/public/app.py
+++ webserver/public/app.py
@@ -33,7 +33,7 @@ def register_page():
 
 @app.route('/recipe/<recipe_id>/')
 def recipe_page(recipe_id):
-  return 'Recipe page with id : {0}'.format(recipe_id)
+  return "recipe page"
 
 #
 # API
@@ -90,72 +90,86 @@ def get_recipes():
         {
             "id": "1",
             "title": "Grilled Cubanos",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2010/06/20100603-cubano-large-thumb-625xauto-93006.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2010/06/20100603-cubano-large-thumb-625xauto-93006.jpg",
+            "score": 0.5
         },
         {
             "id": "2",
             "title": "Cornish Pasty",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/01/01212012-188504-sunday-brunch-cornish-pasty-primary-thumb-625xauto-213242.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/01/01212012-188504-sunday-brunch-cornish-pasty-primary-thumb-625xauto-213242.jpg",
+            "score": 1
         },
         {
             "id": "3",
             "title": "Tomato Avgolemono Soup",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120214-seriousentertaining-soupson-tomatoavegolemenosoup-thumb-625xauto-219164.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120214-seriousentertaining-soupson-tomatoavegolemenosoup-thumb-625xauto-219164.jpg",
+            "score": 1.5
         },
         {
             "id": "4",
             "title": "Cioppino",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/02182012-193155-sunday-supper-cioppino-primary-thumb-625xauto-219621.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/02182012-193155-sunday-supper-cioppino-primary-thumb-625xauto-219621.jpg",
+            "score": 2
         },
         {
             "id": "5",
             "title": "Steak Taco Salad",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120216-193359-dinner-tonight-steak-taco-salad-primary-thumb-625xauto-219875.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120216-193359-dinner-tonight-steak-taco-salad-primary-thumb-625xauto-219875.jpg",
+            "score": 2.5
         },
         {
             "id": "6",
             "title": "Oyakodon",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120219oyakodonlow610-thumb-625xauto-220819.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120219oyakodonlow610-thumb-625xauto-220819.jpg",
+            "score": 3
         },
         {
             "id": "7",
             "title": "Oyakodon",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120219oyakodonlow610-thumb-625xauto-220819.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/02/20120219oyakodonlow610-thumb-625xauto-220819.jpg",
+            "score": 3.5
         },
         {
             "id": "8",
             "title": "Raspberry Limeade",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/06/20120618-lemonade-variations-03-thumb-625xauto-250111.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/06/20120618-lemonade-variations-03-thumb-625xauto-250111.jpg",
+            "score": 4
         },
         {
             "id": "9",
             "title": "Campari Flamingo",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/06/20120621-campari-flamingo-cocktail-primary-thumb-625xauto-251384.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/06/20120621-campari-flamingo-cocktail-primary-thumb-625xauto-251384.jpg",
+            "score": 4.5
         },
         {
             "id": "10",
             "title": "Watermelon Cake",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120709-213358-watermeloncake-thumb-625xauto-254470.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120709-213358-watermeloncake-thumb-625xauto-254470.jpg",
+            "score": 0
         },
         {
             "id": "11",
             "title": "Philly Smash",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/201206-213781-seasonalcocktail-phillysmash-thumb-625xauto-255242.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/201206-213781-seasonalcocktail-phillysmash-thumb-625xauto-255242.jpg",
+            "score": 5
         },
         {
             "id": "12",
             "title": "Kir Royale Sangria",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120710KirRoyaleSangria-thumb-625xauto-255831.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120710KirRoyaleSangria-thumb-625xauto-255831.jpg",
+            "score": 5
         },
         {
             "id": "13",
             "title": "Braided Bread",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120717-214279-bread-baking-braided-bread-thumb-625xauto-256207.jpg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120717-214279-bread-baking-braided-bread-thumb-625xauto-256207.jpg",
+            "score": 5
         },
         {
             "id": "14",
             "title": "Cuban Picadillo",
-            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120719-127677-LatAmCuisine-Picadillo-610x458-thumb-625xauto-257804.jpeg"
+            "image": "http://www.seriouseats.com/recipes/assets_c/2012/07/20120719-127677-LatAmCuisine-Picadillo-610x458-thumb-625xauto-257804.jpeg",
+            "score": 5
         }
     ]
     rand = rd.sample(range(1, 15), 5)
